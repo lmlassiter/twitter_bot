9d2aed65120a2e4a4541f14e8461cfa6a3f73c07
Logan Lassiter <logan@loganlassiter.com>
2023-04-19 11:40:37 -0400

    Update config.py

--- bots/config.py
+++ bots/config.py
@@ -10,8 +10,8 @@ logger = logging.getLogger()
 
 def create_api():
     # Passing info to Tweepy
-    auth = tweepy.OAuthHandler("lcD672yVkNra4cTVwinyc18qG", "ZEAn6FrsC2DUJFD5RgY1gMqXC3KmryfGOZgSfagOzqbAafayun")
-    auth.set_access_token("1562535150969589762-VF6dOMr2b5Vm9BRKfNfp9xflvqqwg6", "ayAvJ6VIWT3oWXieKg1Ry2i7kC0uYZ6algM6XqgAoBgWE")
+    auth = tweepy.OAuthHandler("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+    auth.set_access_token("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
     api = tweepy.API(auth, wait_on_rate_limit=True)
 
     # Verify Connection
